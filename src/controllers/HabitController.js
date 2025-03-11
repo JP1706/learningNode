961f35d52748ea1c45a02301c5586833407b0a1b
JP1706 <jeni86patel@gmail.com>
2025-03-11 10:12:56 +0530

    .

---
+++ src/controllers/HabitController.js
@@ -0,0 +1,43 @@
+const habitModel = require("../models/HabitModel")
+  
+
+const addHabit = async(req,res)=>{
+
+    try{
+        console.log(res.body);
+        const savedHabit = await habitModel.create(req.body)
+        console.log(savedHabit);
+
+        res.status(201).json({
+            message:"habit added sucessfully",
+            data:savedHabit
+            
+        })
+        
+        
+
+    }catch(err){ 
+        res.status(500).json({
+            message:err
+        })
+    }
+}
+
+    const getAllHabits = async(req,res)=>{
+        try{ 
+             
+            const habits = await habitModel.find()
+             res.status(201).json({
+                message:"all habits",
+                data:habits
+             })
+            
+
+        }catch(err){
+            res.status(500).json({
+                message:err
+            })
+        }
+    }
+
+    module.exports = {addHabit,getAllHabits}
--- src/models/HabitsModel.js
+++ src/models/HabitsModel.js
@@ -1,11 +1,32 @@
-const mongoose = require ("mongoose")
-
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const habitSchema = new Schema({
-    screenTime : {
-        required : true,
-        type : Number
+    fullname:{
+        type:String,
+        require:true
     },
-    
+    screenTime:{
+        type:Number,
+        require:true
+    },
+    socialMediaUsage:{
+        type:String,
+        enum:["rarely","occasionally","frequently"],
+        require:true
+    },
+    sleepHours:{
+        type:Number,
+        require:true
+    },
+    physicalActivity:{
+        type:String,
+        require:true,
+        enum:["sedentary","moderate","active"]
+    },
+    additionalComments:{
+        type:String,
+        require:true
+    }
 })
+module.exports = mongoose.model("habit",habitSchema)
