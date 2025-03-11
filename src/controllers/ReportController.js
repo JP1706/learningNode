23c968299fa311356025665a680174c82a72e0cf
JP1706 <jeni86patel@gmail.com>
2025-03-11 18:15:38 +0530

    view report

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -4,38 +4,41 @@ const multer = require("multer")
 
 const CloudinaryUtil = require("../utils/CloudinaryUtil")
 
+// Storage
 const Storage = multer.diskStorage({
-    destination : "./perpetratorsImages",
-    filename : function(req, file, cb) {
+    destination: "./perpetratorsImages",
+    filename: function (req, file, cb) {
         cb(null, file.originalname)
-    } 
+    }
 })
 
+// Multer Object
 const upload = multer({
-    storage : Storage
+    storage: Storage
 }).single("image")
 
+//Post API
 const addReport = async (req, res) => {
     upload(req, res, async (err) => {
-        if(err) {
+        if (err) {
             res.status(500).json({
-                message : err.message
+                message: err.message
             })
         }
         else {
             const CloudinaryResponse = await CloudinaryUtil.uploadFileToCloudinary(req.file)
             req.body.perpetratorImageURL = CloudinaryResponse.secure_url
 
-            try{
+            try {
                 const saveReport = await reportModel.create(req.body)
                 res.status(201).json({
-                    message : "Report Submitted",
-                    data : saveReport
+                    message: "Report Submitted",
+                    data: saveReport
                 })
             }
-            catch(err) {
+            catch (err) {
                 res.status(500).json({
-                    message : err
+                    message: err
                 })
             }
 
@@ -43,6 +46,47 @@ const addReport = async (req, res) => {
     })
 }
 
+// Get API for Particular User
+const getReportsByUserId = async (req, res) => {
+    try {
+        const getReportsByUserId = await reportModel.find({ userId: req.params.userId })
+
+        if (getReportsByUserId.length === 0) {
+            res.status(404).json({ message: "No Reports Found" })
+        }
+        res.status(201).json({
+            message: "Reports Found Successfully",
+            data: getReportsByUserId
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+// Get API for All Users
+const getAllReports = async (req, res) => {
+    try {
+        const Reports = await reportModel.find()
+
+        if (Reports.length === 0) {
+            res.status(404).json({ messsage: "No Reports Found" })
+        }
+
+        res.status(201).json({
+            message: "Reports Found Successfully",
+            data: Reports
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
 module.exports = {
-    addReport
+    addReport, getReportsByUserId, getAllReports
 }
--- src/routes/ReportRoutes.js
+++ src/routes/ReportRoutes.js
@@ -3,5 +3,7 @@ const routes = require("express").Router()
 const reportController = require("../controllers/ReportController")
 
 routes.post("/addReport", reportController.addReport)
+routes.get("/getReportByUserId/:userId", reportController.getReportsByUserId)
+routes.get("/getAllReports", reportController.getAllReports)
 
 module.exports = routes
